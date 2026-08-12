a9103c451d68dcb8797d861338b98a550011f71c
Jona909090 <jonic.ctba@gmail.com>
2026-08-12 22:41:02 +0200

    Polish KOVACNICA clock and ticker

--- src/main.tsx
+++ src/main.tsx
@@ -4,4 +4,5 @@ import { App } from './App';
 import './styles.css';
 import './home.css';
 import './home-refinement.css';
+import './home-polish.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
