65c4351f48965feb27dcaccd52b20d80b6647b95
Jona909090 <jonic.ctba@gmail.com>
2026-08-13 18:59:22 +0200

    Add precise profile dragging

--- src/main.tsx
+++ src/main.tsx
@@ -5,4 +5,5 @@ import './styles.css';
 import './home.css';
 import './home-refinement.css';
 import './home-polish.css';
+import './editor-drag.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
